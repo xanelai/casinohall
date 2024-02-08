d31c11d499771799e6f1bad351e6d2c671305e4b
Felipe Chandía Castillo <felipe@MacBook-Pro-de-Felipe-2.local>
2024-02-08 10:25:32 -0300

    real hall 01

--- components/Scene.js
+++ components/Scene.js
@@ -6,7 +6,7 @@ import { Environment, Stats, OrbitControls, } from '@react-three/drei'
 
 export default function Scene() {
     return <Canvas dpr={[1, 2]} camera={{ fov: 75 }} >
-        <Environment files={'/large_corridor_4k.hdr'} background />
+        <Environment files={'/hall.hdr'} background />
         <directionalLight position={[3.3, 1.0, 4.4]} intensity={4} />
         <OrbitControls target={[0, 1, 0]} autoRotate />
         {/* <axesHelper args={[5]} /> */}
