156589ddb5f55380654d8bf99892f9291eb5bfbf
Felipe Chandía Castillo <felipe@MacBook-Pro-de-Felipe-2.local>
2024-02-13 02:33:41 -0300

    slots

--- components/Scene.js
+++ components/Scene.js
@@ -1,29 +1,70 @@
 import React, { useRef, Suspense, useEffect } from 'react'
-import { Canvas, useLoader, useFrame, useThree } from '@react-three/fiber';
-import { Environment, Stats, OrbitControls, } from '@react-three/drei'
+import { Canvas, useLoader, useFrame, useThree, } from '@react-three/fiber';
+import { Environment, Stats, OrbitControls, Plane, SpotLight } from '@react-three/drei'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+
+
+
+function ModeloFBX(props) {
+    const { position, model } = props;
+    const fbx = useLoader(FBXLoader, '/model.fbx'); // Ruta al archivo FBX
+    useFrame(() => {
+        // Actualizar la posición del modelo FBX en cada fotograma
+        fbx.position.set(props.position[0], props.position[1], props.position[2]);
+    });
+    fbx.receiveShadow = true;
+
+    return <primitive object={fbx} />;
+}
+
+function ModeloFBX2(props) {
+    const { position, model } = props;
+    const fbx = useLoader(FBXLoader, '/model2.fbx'); // Ruta al archivo FBX
+    useFrame(() => {
+        // Actualizar la posición del modelo FBX en cada fotograma
+        fbx.position.set(props.position[0], props.position[1], props.position[2]);
+    });
+    fbx.receiveShadow = true;
+
+    return <primitive object={fbx} />;
+}
+
 
 
 
 
 export default function Scene() {
-    return <Canvas
+    return <Canvas  style={{ background: 'black' }}
         dpr={[1, 2]}
-        camera={{  fov: 95, near: 0.1, far: 1000}}
+        camera={{ position: [0, 1, 7], fov: 95, near: 1, far: 100 }}
         shadows
-        gl={{ antialias: true, toneMapping: 'ReinhardToneMapping', gammaOutput: true }}
+        gl={{ antialias: true }}
+
     >
-        <Environment files={'/hall2.hdr'} background />
-        <directionalLight position={[3.3, 1.0, 4.4]} intensity={4} />
+
+        <ambientLight intensity={1} />
+        <directionalLight position={[0, 5, 5]} intensity={1} castShadow />
+
+        <ModeloFBX position={[0, 0, 0]} />
+        <ModeloFBX2 position={[-3, 0, 0]} />
+
+        {/* Dibujar un suelo */}
+        <Plane args={[100, 100]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
+            <shadowMaterial attach="material" opacity={0.9} />
+            <meshBasicMaterial attach="material" color="#b71c1c" />
+        </Plane>
+        <SpotLight position={[2, 10, 0]} intensity={60} angle={Math.PI / 5} penumbra={0} castShadow />
+        <SpotLight position={[0, 10, 0]} intensity={60} angle={Math.PI / 5} penumbra={0} castShadow />
+        <SpotLight position={[-2, 10, 0]} intensity={60} angle={Math.PI / 5} penumbra={0} castShadow />
+
         <OrbitControls />
+
+
+
+
 
         {/* <axesHelper args={[5]} /> */}
         <Stats />
-        <RotatingCube />
-
-
-
-
-
 
     </Canvas>
 
--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,14 @@
 import React, { useEffect, useRef, useState } from 'react'
 import Scene from "@/components/Scene";
 
-import { CubeTextureLoader } from 'three';
-
-
-
-
 
 export default function Home() {
-  const [envMap, setEnvMap] = useState(null);
-  useEffect(() => {
-    const loadEnvMap = async () => {
-      const map = await new CubeTextureLoader().loadAsync(['/large_corridor_4k.hdr']);
-      setEnvMap(map);
-    };
-    loadEnvMap();
-  }, []);
+
 
   return (
     <>
       <div style={{ height: '100vh', overflow: 'hidden' }}>
-        <Scene >
-          
-          
-        </Scene>
+        <Scene />
       </div>
     </>
   );
